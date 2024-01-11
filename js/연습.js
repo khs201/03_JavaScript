72c5ca715766ff0037ae056682f1ab330e113e6d
khs <hanseog201@naver.com>
2024-01-11 17:03:37 +0900

    240111_자바첫쉅끝

---
+++ js/연습.js
@@ -0,0 +1,8 @@
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const num3 = document.getElementById("num3");
+const total = document.getElementById("total")
+
+function totalFn(){
+    const value1
+}
