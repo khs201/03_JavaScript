9335880535ae73205d2c75755e0a44bae9ab1e01
khs <hanseog201@naver.com>
2024-01-26 17:23:03 +0900

    240126_초안완성

--- js/예제1.js
+++ js/예제1.js
@@ -1,5 +1,6 @@
 resName = document.querySelector("#resName");
 resInput = document.querySelector("#resInput");
+list = document.querySelector("#list");
 fixBtn = document.querySelector("#fixBtn");
 addBtn = document.querySelector("#addBtn");
 finBtn = document.querySelector("#finBtn");
@@ -39,51 +40,96 @@ fixBtn.addEventListener("click", e => {
     delBtn.style.display = "block"; // 삭제 버튼 보이게
     finBtn.style.display = "block"; // 종료 버튼 보이게
 
+
+
     menuList.forEach((menu) => {
+
         const text = menu.firstChild.textContent.trim();
-        const number = menu.querySelector('span').textContent;
-        menu.innerHTML = '<input type="text" value="' + text + '"><input type="number" value="' + number + '">';
-    });
-    finBtn.addEventListener("click", e => {
-        const menuList = document.querySelectorAll("li");
-        const menuNameList = document.querySelectorAll("li>input[type='text']");
-        const menuPriceList = document.querySelectorAll("li>input[type='number']");
-        menuNameList.forEach((menuName) => {
-            const textInput = menuName.value;
-            console.log(menuName.value);
-        });
+        // const text = menu.childNodes[1].textContent.trim();
+        const number = Number(menu.querySelector('span').textContent);
+        menu.innerHTML = '<input type="checkbox" id="check"><input type="text" value="' + text + '" class="menuInput"><input type="text" value="' + number + '"class = "priceInput">';
     });
 
-    /* finBtn.addEventListener("click", e => {
-        const menuList = document.querySelectorAll("li");
-        const menuNameList = document.querySelectorAll("li>input[type='text']");
-        const menuPriceList = document.querySelectorAll("li>input[type='number']");
-    
-        menuNameList.forEach((menuName, index) => {
-            const textInput = menuName.value;
-            const numberInput = menuPriceList[index].value;
-            menuList[index].innerHTML = '<span>' + textInput + '</span><span>' + numberInput + '</span>';
-        });
-    
-        fixBtn.style.display = "block"; // 수정 버튼 보이게
-        addBtn.style.display = "none"; // 추가 버튼 안 보이게
-        delBtn.style.display = "none"; // 삭제 버튼 안 보이게
-        finBtn.style.display = "none"; // 종료 버튼 안 보이게
-    }); */
+
+
+
+
+});
+
+finBtn.addEventListener("click", e => {
+    const menuList = document.querySelectorAll("li");
+    const menuNameList = document.querySelectorAll(".menuInput");
+    const menuPriceList = document.querySelectorAll(".priceInput");
+
+
+
+    menuList.forEach((menu, index) => {
+
+        if (menuNameList[index] && menuPriceList[index]) {
+            const menuName = menuNameList[index].value;
+            const menuPrice = menuPriceList[index].value;
+
+            if (menuName == "" || menuPrice == "") {
+                // menu.parentNode.parentNode.removeChild;
+                menu.parentNode.removeChild(menu);
+            } else {
+                // 새로운 li 태그 생성
+                const newLi = document.createElement("li");
+                newLi.innerHTML = `${menuName}<span>${menuPrice}</span>원`;
+                // 기존 li 태그를 새로운 li 태그로 교체
+                menu.parentNode.replaceChild(newLi, menu);
+            }
+        }
+
+
+
+        /* else {
+            console.log(`menuNameList[${index}] or menuPriceList[${index}] is undefined`);
+        } */
+
+    });
+
+
+    fixBtn.style.display = "block"; // 수정 버튼 보이게
+    addBtn.style.display = "none"; // 추가 버튼 안 보이게
+    delBtn.style.display = "none"; // 삭제 버튼 안 보이게
+    finBtn.style.display = "none"; // 종료 버튼 안 보이게
+
+
+
 });
 
 
 
+addBtn.addEventListener("click", e => { // ***** 경고 출력 [Violation]'click' handler took 1207ms *******
+    const menu = document.querySelectorAll("#list>li");
+    if (menu.length == 15) { // 15개 까지 생성 제한
+        alert("15개 까지만");
+        return;
+    };
+    const addLi = document.createElement("li");
+    addLi.innerHTML = `<input type="checkbox" id="check"><input type="text" placeholder="메뉴명" class="menuInput"><input type="text" placeholder="가격" class="priceInput"></input>`
+    list.append(addLi);
+});
+
+// delBtn.addEventListener("click", e => {
+//     document.querySelectorAll("#check");
 
 
-// document.querySelector('button').addEventListener('click', function() {
-//     var lis = document.querySelectorAll('li');
-//     lis.forEach(function(li) {
-//         var text = li.firstChild.nodeValue.trim();
-//         var number = li.querySelector('span').innerText;
-//         li.innerHTML = '<input type="text" value="' + text + '"><input type="number" value="' + number + '">';
-//     });
+
 // });
+delBtn.addEventListener("click", e => {
+    const checkboxes = document.querySelectorAll("#check");
+
+    checkboxes.forEach((checkbox) => {
+        if (checkbox.checked) {
+            checkbox.parentNode.remove();
+        }
+    });
+});
+
+
+
 
 
 
--- 메뉴판관리.js
+++ 메뉴판관리.js
@@ -151,3 +151,5 @@ deleteMenu.addEventListener("click", e => {
     item.parentElement.remove();
   });
 });
+
+
